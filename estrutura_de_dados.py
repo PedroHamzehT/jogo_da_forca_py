f36ad2ea1c88c34d7c14349346ab8e92b3d3cedf
PedroHamzehT <pedrohamzeh@gmail.com>
2019-08-02 17:47:49 -0300

    Ultimo exercicio falta resolver problema do loop estar sobrescrevendo o dicionario

--- estrutura_de_dados.py
+++ estrutura_de_dados.py
@@ -1,3 +1,5 @@
+import pdb
+
 # Estrutura de dados Ex 1
 any_list = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8, 3, 3, -52]
 print('O maior valor da lista eh: {}'.format(max(any_list)))
@@ -28,4 +30,30 @@ user_datas.append(input('Digite seu sobrenome por favor: '))
 user_datas.append(input('Digite sua idade por favor: '))
 for data in user_datas:
     print(data)
-print('*' * 20)
+
+# Estrutura de dados Ex 3
+print('*' * 50)
+user_grades = []
+user_grades.append(float(input('Digite a primeira nota: ')))
+user_grades.append(float(input('Digite a segunda nota: ')))
+user_grades.append(float(input('Digite a terceira nota: ')))
+user_grades.append(float(input('Digite a quarta nota: ')))
+user_average = sum(user_grades)/len(user_grades)
+print('A media do aluno foi: {}'.format(round(user_average, 2)))
+
+# Estrutura de dados Ex 4 e Ex 5
+print('*' * 50)
+user_infos = {}
+users = []
+while True:
+    pdb.set_trace()
+    user_infos['name'] = input('Digite o seu nome por favor: ')
+    user_infos['age'] = input('Digite a sua idade por favor: ')
+    user_infos['city'] = input('Digite a cidade onde mora por favor: ')
+    users.append(user_infos)
+    if(input('Quer adicionar outro usuario? (S/N) ').upper() == 'N'):
+        break
+for user in users:
+    print('{}: {}'.format('nome', user['name']))
+    print('{}: {}'.format('idade', user['age']))
+    print('{}: {}'.format('cidade', user['city']))
